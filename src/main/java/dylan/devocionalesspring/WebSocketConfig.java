b7d0f58f6aa4f2ad1d03d2ea6c589df510aa022b
Dylan Fernandez <127891562+Dynezel@users.noreply.github.com>
2025-07-30 18:52:44 -0300

    Update WebSocketConfig a redis.java

--- src/main/java/dylan/devocionalesspring/WebSocketConfig.java
+++ src/main/java/dylan/devocionalesspring/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package dylan.devocionalesspring;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServletServerHttpRequest;
@@ -17,30 +16,11 @@ import java.util.Map;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Value("${spring.rabbitmq.host}")
-    private String rabbitHost;
 
-    @Value("${spring.rabbitmq.port}")
-    private int rabbitPort;
-
-    @Value("${spring.rabbitmq.username}")
-    private String username;
-
-    @Value("${spring.rabbitmq.password}")
-    private String password;
-    @Value("${spring.rabbitmq.virtual-host}")
-    private String virtualHost;
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableStompBrokerRelay("/topic", "/queue", "/user")
-                .setRelayHost(rabbitHost)
-                .setRelayPort(rabbitPort)
-                .setClientLogin(username)
-                .setClientPasscode(password)
-                .setVirtualHost(virtualHost)
-                .setSystemLogin(username)
-                .setSystemPasscode(password);
-
+        // Broker embebido, el mensaje sale por /queue o /topic
+        registry.enableSimpleBroker("/topic", "/queue", "/user");
         registry.setApplicationDestinationPrefixes("/app");
         registry.setUserDestinationPrefix("/user");
     }
@@ -49,7 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-notifications")
                 .setAllowedOrigins("https://devocionales-app-frontend.vercel.app")
-                .setHandshakeHandler(new CustomHandshakeHandler()) // ← ESTA LÍNEA ES CLAVE
+                .setHandshakeHandler(new CustomHandshakeHandler())
                 .withSockJS();
     }
 
