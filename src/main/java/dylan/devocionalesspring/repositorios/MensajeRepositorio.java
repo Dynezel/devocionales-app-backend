a104f8ca3205a9ead80d5acffc6554190c8f5b76
Dylan Fernandez <127891562+Dynezel@users.noreply.github.com>
2025-08-14 18:50:31 -0300

    Imports actualizados MensajeRepositorio.java

--- src/main/java/dylan/devocionalesspring/repositorios/MensajeRepositorio.java
+++ src/main/java/dylan/devocionalesspring/repositorios/MensajeRepositorio.java
@@ -2,6 +2,8 @@ package dylan.devocionalesspring.repositorios;
 
 import dylan.devocionalesspring.entidades.Mensaje;
 import dylan.devocionalesspring.entidades.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -27,7 +29,7 @@ public interface MensajeRepositorio extends JpaRepository<Mensaje, Long> {
             @Param("emisorId") Long emisorId,
             @Param("receptorId") Long receptorId,
             Pageable pageable);
-    
+
     //Query para buscar la conversacion entre ambos usuarios
     @Query("SELECT m FROM Mensaje m WHERE (m.emisor.id = :emisorId AND m.receptor.id = :receptorId) OR (m.emisor.id = :receptorId AND m.receptor.id = :emisorId) ORDER BY m.fechaEnvio ASC")
     List<Mensaje> findByUsuarios(@Param("emisorId") Long emisorId, @Param("receptorId") Long receptorId);
@@ -35,6 +37,5 @@ public interface MensajeRepositorio extends JpaRepository<Mensaje, Long> {
     void deleteByEmisor(Usuario emisor);
 
     void deleteByReceptor(Usuario receptor);
-
 
 }
